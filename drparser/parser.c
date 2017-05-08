cac92d820a41f876ae93acb5804452c7b3a77b57
Tormenta <diego.sainzdemedrano@gmail.com>
2017-05-08 20:16:05 +0100

    Initial clean-up.

---
+++ drparser/parser.c
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include "format.h"
+#include "parser.h"
+
+int line=1;
+extern YYSTYPE yylval;
+
+/** auxiliar **/
+
+bool is_decl_type(int token) {
+    return 
+        token == INT ||
+        token == LONG ||
+        token == UINT ||
+        token == CHAR ||
+        token == STRING ||
+        token == FLOAT  ||
+        token == DOUBLE;
+}
+
+/** implementations **/
+
+int parse() {
+    /* TODO: code generation in this line */
+    return program();
+    /* and this one */
+}
+
+int program() {
+    int next;
+    while(is_decl_type(next = yylex()) || next == FUNC)
+        if(!declaration() && !definition())
+            return PARSE_ERR;
+    return mn();
+}
+
+int declaration() {
+    //int next = yylex();
+    return PARSE_ERR;
+}
+
+int definition() {
+    return PARSE_ERR;
+}
+
+int mn() {
+    return PARSE_ERR;
+}
+
+int assignment() {
+    return PARSE_ERR;
+}
+
+int nexp() {
+    return PARSE_ERR;
+}
+
+int bexp() {
+    return PARSE_ERR;
+}
+
+int signature() {
+    return PARSE_ERR;
+}
+
+int main_sig() {
+    return PARSE_ERR;
+}
+
+int argument() {
+    return PARSE_ERR;
+}
+
+int code() {
+    return PARSE_ERR;
+}
+
+int call() {
+    return PARSE_ERR;
+}
+
+int parameter() {
+    return PARSE_ERR;
+}
+
+void verbose(const char* msg) {
+    char buffer[256] = "";
+    sprintf(buffer,"parser@%i: %s\n", line, msg);
+    fprintf(stdout,buffer);
+}
+
+void p_error(const char* msg) {
+    fprintf(stderr,"parser@%i:" RED " %s\n" CLEAR_FORMAT,line,msg);
+}
+
